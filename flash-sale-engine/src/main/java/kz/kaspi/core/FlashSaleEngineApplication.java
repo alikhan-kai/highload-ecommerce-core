79812afa694f62eb9f1a24a9102f1c2c727a2d4a
Alikhan-kai <alikhannurlanov.280207@gmail.com>
2026-09-13 21:30:53 +0500

    Fix: add flash-sale-engine as regular files

---
+++ flash-sale-engine/src/main/java/kz/kaspi/core/FlashSaleEngineApplication.java
@@ -0,0 +1,13 @@
+package kz.kaspi.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashSaleEngineApplication.class, args);
+    }
+
+}
