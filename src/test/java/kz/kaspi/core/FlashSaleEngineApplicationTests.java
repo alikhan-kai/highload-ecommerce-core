0b41d322832438bc2509591716100ae22925947e
Alikhan-kai <alikhannurlanov.280207@gmail.com>
2026-09-13 21:27:42 +0500

    Initial commit

---
+++ src/test/java/kz/kaspi/core/FlashSaleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kaspi.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashSaleEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
